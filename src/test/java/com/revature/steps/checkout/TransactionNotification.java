531a44d5e2f2f9ae6d014339e99d2634374cc959
Jonathan Whitmire <jkwhitmi@gmail.com>
2022-12-06 16:21:19 -0600

    added some features to split up the shipping information inputs into multiple steps, and added a given for the login information

--- src/test/java/com/revature/steps/checkout/TransactionNotification.java
+++ src/test/java/com/revature/steps/checkout/TransactionNotification.java
@@ -11,8 +11,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TransactionNotification {
 
-    @Given("The user has added an item to their cart")
-    public void the_user_has_added_an_item_to_their_cart() {
+    @Given("The User has logged in")
+    public void the_user_has_logged_in() {
         MainRunner.driver.get("http://localhost:4200/");
         MainRunner.wait.until(ExpectedConditions.visibilityOf(MainRunner.homePage.signInLink));
         MainRunner.homePage.signInLink.click();
@@ -22,6 +22,9 @@ public class TransactionNotification {
         MainRunner.loginPage.password.sendKeys("guest");
         MainRunner.wait.until(ExpectedConditions.visibilityOf(MainRunner.loginPage.loginButton));
         MainRunner.loginPage.loginButton.click();
+    }
+    @Given("The user has added an item to their cart")
+    public void the_user_has_added_an_item_to_their_cart() {
         Actions action = new Actions(MainRunner.driver);
         WebElement firstProduct = MainRunner.wait.until(ExpectedConditions.visibilityOf(MainRunner.homePage.firstProduct));
         action.moveToElement(firstProduct).perform();
@@ -32,14 +35,32 @@ public class TransactionNotification {
     public void the_user_clicks_the_checkout_button_from_the_cart_link() {
         MainRunner.yourBagPage.checkoutNowBtn.click();
     }
-    @When("The user fills out the shipping information")
-    public void the_user_fills_out_the_shipping_information() {
+    @When("The user enters a First name")
+    public void the_user_enters_a_first_name() {
         MainRunner.checkoutPage.firstNameInput.sendKeys("testfname");
+    }
+    @When("The user enters a Last name")
+    public void the_user_enters_a_last_name() {
         MainRunner.checkoutPage.lastNameInput.sendKeys("testlname");
+    }
+    @When("The user enters an Address")
+    public void the_user_enters_an_address() {
         MainRunner.checkoutPage.addressInput.sendKeys("101 Test Lane");
+    }
+    @When("The user enters a city")
+    public void the_user_enters_a_city() {
         MainRunner.checkoutPage.cityInput.sendKeys("Testville");
+    }
+    @When("The user enters a State Province or Region")
+    public void the_user_enters_a_state_province_region() {
         MainRunner.checkoutPage.stateInput.sendKeys("Testississippi");
+    }
+    @When("The user enters a Zip or Postal code")
+    public void the_user_enters_a_zip_postal_code() {
         MainRunner.checkoutPage.zipInput.sendKeys("86753");
+    }
+    @When("The user enters a Country")
+    public void the_user_enters_a_country() {
         MainRunner.checkoutPage.countryInput.sendKeys("Canada");
     }
     @When("The user clicks next")
--- src/test/resources/features/notification/transaction-notificaiton.feature
+++ src/test/resources/features/notification/transaction-notificaiton.feature
@@ -1,9 +1,16 @@
 Feature: A user should receive a notification when a transaction occurred
 
   Scenario: Notification following transaction
+    Given The User has logged in
     Given The user has added an item to their cart
     When The user clicks the checkout button from the cart link
-    And The user fills out the shipping information
+    And The user enters a First name
+    And The user enters a Last name
+    And The user enters an Address
+    And The user enters a city
+    And The user enters a State Province or Region
+    And The user enters a Zip or Postal code
+    And The user enters a Country
     And The user clicks next
     And The user selects a payment method
     And The user clicks the submit payment button
