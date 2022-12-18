136023c9aaa620bf84b0c403c16dee16425fa2c3
David Hanzlik <david420@revature.net>
2022-12-18 16:02:10 -0600

    the rest of the runners appears to be working

--- src/test/java/com/revature/steps/dm/DarkMode.java
+++ src/test/java/com/revature/steps/dm/DarkMode.java
@@ -1,5 +1,8 @@
 package com.revature.steps.dm;
 
+import com.revature.Urls;
+import com.revature.pages.HomePage;
+import com.revature.pages.LoginPage;
 import com.revature.steps.SeleniumSteps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -7,19 +10,23 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class DarkMode extends SeleniumSteps {
-    @Given("The registeredUser is logged in and on the home page")
+    @Given("The user is logged in and on the home page")
     public void the_registered_user_is_logged_in_and_on_the_home_page() {
-        driver.get("http://localhost:4200/");
-        wait.until(ExpectedConditions.visibilityOf(homePage.signInLink));
+        driver.get(Urls.base);
+        wait.until(ExpectedConditions.urlMatches(Urls.base + "/?"));
+        homePage = new HomePage(driver);
         homePage.signInLink.click();
-        wait.until(ExpectedConditions.visibilityOf(loginPage.email));
+        wait.until(ExpectedConditions.urlMatches(Urls.login + "/?"));
+        loginPage = new LoginPage(driver);
         loginPage.email.sendKeys("test@example.com");
         wait.until(ExpectedConditions.visibilityOf(loginPage.password));
         loginPage.password.sendKeys("guest");
         wait.until(ExpectedConditions.visibilityOf(loginPage.loginButton));
         loginPage.loginButton.click();
+        wait.until(ExpectedConditions.urlMatches(Urls.base + "/?"));
+        homePage = new HomePage(driver);
     }
-    @When("The registeredUser clicks the dark mode checkbox")
+    @When("The user clicks the dark mode checkbox")
     public void the_registered_user_clicks_the_dark_mode_checkbox() {
         homePage.darkModeBtn.click();
         homePage.darkModeBtn.click();
@@ -31,11 +38,11 @@ public class DarkMode extends SeleniumSteps {
             return darkBody.equals("rgba(18, 18, 18, 1)");
         });
     }
-    @When("The registeredUser clicks the profile link")
+    @When("The user clicks the profile link")
     public void the_registered_user_clicks_the_profile_link() {
         homePage.profileLink.click();
     }
-    @When("The registeredUser clicks the dark mode checkbox again")
+    @When("The user clicks the dark mode checkbox again")
     public void the_registered_user_clicks_the_dark_mode_checkbox_again() {
         homePage.darkModeBtn.click();
     }
@@ -46,25 +53,6 @@ public class DarkMode extends SeleniumSteps {
             return darkBody.equals("rgba(255, 255, 255, 1)");
         });
     }
-    @Given("The guest is logged in and on the home page")
-    public void the_guest_is_logged_in_and_on_the_home_page() {
-        driver.get("http://localhost:4200/");
-    }
-    @Given("The guest is on the home page")
-    public void the_guest_is_on_the_home_page() { driver.get("http://localhost:4200/"); }
-    @When("The guest clicks the dark mode checkbox")
-    public void the_guest_clicks_the_dark_mode_checkbox() {
-        homePage.darkModeBtn.click();
-        homePage.darkModeBtn.click();
-    }
-    @When("The guest clicks the dark mode checkbox again")
-    public void the_guest_clicks_the_dark_mode_checkbox_again() {
-        homePage.darkModeBtn.click();
-    }
-    @When("The guest clicks the profile link")
-    public void the_guest_clicks_the_profile_link() {
-        homePage.profileLink.click();
-    }
     @Then("The screen is still in dark mode")
     public void the_screen_is_still_in_dark_mode() {
         wait.until(driver -> {
@@ -72,12 +60,8 @@ public class DarkMode extends SeleniumSteps {
             return darkBody.equals("rgba(18, 18, 18, 1)");
         });
     }
-    @When("The registeredUser clicks the cart link")
+    @When("The user clicks the cart link")
     public void the_registered_user_clicks_the_cart_link() {
-        homePage.navCartLink.click();
-    }
-    @When("The guest clicks the cart link")
-    public void the_guest_clicks_the_cart_link() {
         homePage.navCartLink.click();
     }
 }
--- src/test/resources/features/dark-mode/dark-mode-positive.feature
+++ src/test/resources/features/dark-mode/dark-mode-positive.feature
@@ -1,30 +1,25 @@
 @dark-mode
 Feature: A registered user, guest, or admin can adjust color scheme to normal or dark mode
 
-  Scenario Outline: A registered user, guest, or admin can change from normal to dark mode and vice versa on the home page
-    Given The <user> is logged in and on the home page
-    When The <user> clicks the dark mode checkbox
+  Scenario: A registered user, guest, or admin can change from normal to dark mode and vice versa on the home page
+    Given The user is logged in and on the home page
+    When The user clicks the dark mode checkbox
     Then The screen changes from normal to dark mode
-    When The <user> clicks the dark mode checkbox again
+    When The user clicks the dark mode checkbox again
     Then The screen changes from dark mode to normal
 
-    Examples:
-    | user            |
-    | registeredUser  |
-    | guest           |
-
   Scenario: The dark mode color scheme is enable for each registered user link on the web page
-    Given The registeredUser is logged in and on the home page
-    When The registeredUser clicks the dark mode checkbox
+    Given The user is logged in and on the home page
+    When The user clicks the dark mode checkbox
     Then The screen changes from normal to dark mode
-    When The registeredUser clicks the profile link
+    When The user clicks the profile link
     Then The screen is still in dark mode
-    When The registeredUser clicks the cart link
+    When The user clicks the cart link
     Then The screen is still in dark mode
 
   Scenario: The dark mode color scheme is enable for each guest link on the web page
-    Given The guest is on the home page
-    When The guest clicks the dark mode checkbox
+    Given The user is on the home page
+    When The user clicks the dark mode checkbox
     Then The screen changes from normal to dark mode
-    When The guest clicks the cart link
+    When The user clicks the cart link
     Then The screen is still in dark mode
